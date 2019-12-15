d2edbbaf3dafe389b24a5492834f07ba8d093570
Pedro Souza <pesouza.civil@gmail.com>
2019-12-15 13:57:17 -0300

    AuthRepository e AuthController
    Foi criado a interface IAuthRepository como implementação do design pattern Repository, alem disso foi criado um authController para atender as requisições de autenticação +segunda migração

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repository;
+
+        public AuthController(IAuthRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            //validate request
+
+            username = username.ToLower();
+            if (await _repository.UserExists(username))
+            {
+                return BadRequest("Username already exists.");
+            }
+
+            var userToCreate = new User
+            {
+                UserName = username
+            };
+
+            var createdUser = await _repository.Register(userToCreate, password);
+
+            return StatusCode(201);
+        }
+    }
+}
