ea15b7251977038f67af8f202c29e43b19ff0303
Pedro Souza <pesouza.civil@gmail.com>
2019-12-31 11:17:07 -0300

    Error Handler
    foi adicionado alguns componentes no SPA e classes na API para lidar com as execptions

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using Microsoft.AspNetCore.Authorization;
@@ -43,18 +41,21 @@ namespace DatingApp.API.Controllers
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            // Method intentionally left empty.
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            // Method intentionally left empty.
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            // Method intentionally left empty.
         }
     }
 }
